10f37b531aa46cf65b1e230fdb1a0911272d3fcf
josue <josue.tamayo@cbtis72.edu.mx>
2019-05-18 20:15:37 -0500

    Commit de roch jaja

---
+++ app/src/main/java/com/example/tablaperiodica/Screen.java
@@ -0,0 +1,26 @@
+package com.example.tablaperiodica;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent intent =  new Intent(Screen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 1500);
+    }
+    }
+
